47d9bc9981ad189c7775d143580a1b8c3e0d11c3
prose15 <prosemartial@gmail.com>
2025-09-07 01:03:38 +0530

    bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { config } from './wagmiConfig.js'
 import { Provider } from 'react-redux'
+import { store } from './redux/store.js'
 
 const queryClient = new QueryClient();
 
@@ -15,7 +16,7 @@ createRoot(document.getElementById('root')).render(
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
        <RainbowKitProvider>
-        <Provider>
+        <Provider store={store}>
          <App />
         </Provider>
        </RainbowKitProvider>
